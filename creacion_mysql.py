54fd57d8dee5a05738b8ac674ab77abd846f67bd
JUJO79 <juan.lopez100@udea.edu.co>
2024-12-09 10:47:43 -0500

    Update creacion_mysql.py
    Conexión con base de datos

--- creacion_mysql.py
+++ creacion_mysql.py
@@ -1,1 +1,30 @@
+import mysql.connector
+import os
+cnx = mysql.connector.connect(user='root' , passwd='password', host='localhost')
+cursor = cnx.cursor()
 
+# cursor.execute("CREATE DATABASE info2")
+cursor.execute("USE info2")
+
+cursor.execute("DROP TABLE IF EXISTS usuarios")
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS usuarios (
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  nombre_usuario VARCHAR(40) UNIQUE NOT NULL,
+  nombre VARCHAR(40) NOT NULL,
+  contrasena VARCHAR(40) NOT NULL
+  )
+""")
+
+cursor.execute("""
+INSERT INTO usuarios VALUES
+(1, 'camila', 'Camila ... ... ...', '123456'),
+(2, 'aleja', 'Aleja ... ... ...', '654321')
+""")
+
+# cursor.execute("SELECT * FROM usuarios")
+
+# print(cursor.fetchall())
+
+cnx.commit()
+cnx.close()
